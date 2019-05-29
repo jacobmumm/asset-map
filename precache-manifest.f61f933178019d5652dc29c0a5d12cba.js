9753184d825ce380d95957bc1beccb9cd5f2b13d
Tomas Maly <tmaly1980@gmail.com>
2019-05-29 15:35:47 -0400

    saving

---
+++ precache-manifest.f61f933178019d5652dc29c0a5d12cba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/asset-map/robots.txt"
+  },
+  {
+    "revision": "2551b8cdf0d5a052f35e",
+    "url": "/asset-map/js/chunk-vendors.76def454.js"
+  },
+  {
+    "revision": "7d0ba080403746a7a941",
+    "url": "/asset-map/js/app.c783ac94.js"
+  },
+  {
+    "revision": "c0123fa57896eed88b8f",
+    "url": "/asset-map/js/about.ac4b0521.js"
+  },
+  {
+    "revision": "0824d26fa8d61e5451c2ef66c53d714e",
+    "url": "/asset-map/index.html"
+  },
+  {
+    "revision": "2551b8cdf0d5a052f35e",
+    "url": "/asset-map/css/chunk-vendors.bf2de2ff.css"
+  },
+  {
+    "revision": "7d0ba080403746a7a941",
+    "url": "/asset-map/css/app.11c72b5e.css"
+  }
+];
