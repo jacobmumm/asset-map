b1bbaa63af83762976a0448fd79bad4d9aa677a7
Tomas Maly <tmaly1980@gmail.com>
2019-05-29 14:49:51 -0400

    Saving path

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 module.exports = {
-  publicPath: "asset-map/",
+  publicPath: "asset-map/dist",
   pluginOptions: {
     'style-resources-loader': {
       preProcessor: 'scss',
